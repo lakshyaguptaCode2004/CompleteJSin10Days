63c651756d73baa46fe3e2c9f4767bb892d75d46
Lakshya Gupta <lakshyachatgpt1@gmail.com>
2025-10-24 17:41:18 +0530

    add 03_conversionOperations.js

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,41 @@
+// let score="33"
+// let valueInNumber=Number(score)
+// console.log(score+3)
+// console.log(valueInNumber+3)
+///
+
+//Operations
+
+
+// console.log(2/3)
+
+
+
+// let str1="hello"
+// let str2=" world"
+// console.log(str1+str2)
+
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log(1+"2"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(1+2+"2"+"3")
+
+// console.log(+true)
+// console.log(+"")
+
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+
+let gameCounter=100
+console.log(gameCounter)
+++gameCounter
+console.log(gameCounter)
+console.log(++gameCounter)
+console.log(gameCounter++)
+console.log(gameCounter)
+
+
