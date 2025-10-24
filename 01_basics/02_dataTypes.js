bb27595478216f21c83756017db740b2dcac5c65
Lakshya Gupta <lakshyachatgpt1@gmail.com>
2025-10-24 17:39:53 +0530

    add 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict";
+const { type } = require("express/lib/response");
+
+//treat all Js code as new version/js;
+
+//   alert(3+3)   we are using nodejs , not browser
+let name="Hitesh"
+let age=18
+let isLoggedin=false
+console.log(typeof null)
+console.log(typeof undefined)
