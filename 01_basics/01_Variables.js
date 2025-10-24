710e6fbd58c5b63c5ea8b89f5f9d6377eba8cb41
Lakshya Gupta <lakshyachatgpt1@gmail.com>
2025-10-24 17:37:44 +0530

    Add 01_variables.js

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,8 @@
+const accountId=14223
+let accountEmail="lakshyastudycolleg@gmail.com"
+var accountPassword="Alwar@123"//dont use it nowadays due to problem of block scope and functional scope
+accountCity="Jaipur"//it is a bad practice
+
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
